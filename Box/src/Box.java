47dcb15be26eaf0ffcf75bb1ce7b82bedfe0d1eb
Amir Hamza <123008407+amir160s@users.noreply.github.com>
2023-02-05 23:06:49 +0600

    Add files via upload
    Create Box class (variable: height, width, length; method: calculate_area()) and its object

---
+++ Box/src/Box.java
@@ -0,0 +1,8 @@
+public class Box {
+    int height,width,length;
+    void calculate_area()
+    {
+        int area=height*width*length;
+        System.out.println(area);
+    }
+}
---
+++ Box/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Box box1=new Box();
+        box1.length=5;
+        box1.width=5;
+        box1.height=3;
+        box1.calculate_area();
+    }
+}
