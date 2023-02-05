3b04faa1108e2f6d87f2eafeae00e30d639afb18
Amir Hamza <123008407+amir160s@users.noreply.github.com>
2023-02-05 23:08:05 +0600

    Add files via upload
    Create Employee class (variable: name, salary; method: display()) and its object

---
+++ Employee Salary/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee s= new Employee();
+        s.salary=600000;
+        s.name="Amir";
+        s.display();
+    }
+}
