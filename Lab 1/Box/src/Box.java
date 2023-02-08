904d5cc81e8f676b048f75fd6cef36e6cc794330
Amir Hamza <123008407+amir160s@users.noreply.github.com>
2023-02-08 15:47:42 +0600

    Add files via upload
    Lab Class Task 1

---
+++ Lab 1/Box/src/Box.java
@@ -0,0 +1,10 @@
+public class Box {
+    int height;
+    int width;
+    int length;
+    void calculate_area()
+    {
+        int area=height*width*length;
+        System.out.println(area);
+    }
+}
